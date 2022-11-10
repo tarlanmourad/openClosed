5a97eda229e7b3b13ccd0f8f4f734792d376bcee
Tarlan Muradli <terlanmuradli075@mail.ru>
2022-11-10 11:21:47 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace openClosed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSql = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSql
+            // 
+            this.btnSql.Location = new System.Drawing.Point(68, 29);
+            this.btnSql.Name = "btnSql";
+            this.btnSql.Size = new System.Drawing.Size(111, 42);
+            this.btnSql.TabIndex = 0;
+            this.btnSql.UseVisualStyleBackColor = true;
+            this.btnSql.Click += new System.EventHandler(this.btnSql_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(74, 98);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 16);
+            this.lblResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 155);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSql);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSql;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace openClosed
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source=TARLAN\\LOCALHOST;Initial Catalog=Northwind;Integrated Security=True");
+
+        private void btnSql_Click(object sender, EventArgs e)
+        {
+            //conn.Open();
+            Check();
+
+            if (conn.State != ConnectionState.Open)
+            {
+                btnSql.Text = "Connect";
+            }
+            else if (conn.State == ConnectionState.Open)
+            {
+                btnSql.Text = "Disconnect";
+            }
+            else
+            {
+                lblResult.Text = "Problem in connection";
+            }
+        }
+
+
+        private void Check()
+        {
+            if (conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+                lblResult.Text = "Connected to Db";
+            }
+            else if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+                lblResult.Text = "Disconnected";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnSql.Text = "Disconnect";
+            Check();
+        }
+    }
+}
